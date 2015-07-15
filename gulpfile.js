085f983a75b3c15c88fdeb1a4a0bb34461320dd8
Magrippinho <magrippinho@gmail.com>
2015-07-15 21:10:20 +0300

    Sass styling, toolbar tooltips.

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,9 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.styles(['angular-material/angular-material.min.css'], 'public/assets/css/vendor.css', 'node_modules')
+    mix.styles(['angular-material/angular-material.min.css'], 'resources/assets/css/vendor.css', 'node_modules')
+        .sass('magrippis.scss', 'resources/assets/css/magrippis.css')
+        .styles(['vendor.css', 'magrippis.css'], 'public/assets/css/magrippis.css')
         .browserify('angular/app.js', 'public/assets/js')
-        .version('assets/js/bundle.js');
+        .version(['assets/js/bundle.js', 'assets/css/magrippis.css']);
 });
--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -4,45 +4,79 @@
     <meta charset="UTF-8">
     <title>Johnny Magrippis: Developer, designer, determined gamer</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/vendor.css">
+    <link rel="stylesheet" href="{{ elixir('assets/css/magrippis.css') }}">
 </head>
 <body>
 <div ng-app="magrippis" ng-controller="HomeController as homeC" layout="column">
     <md-toolbar>
         <div class="md-toolbar-tools">
-            <md-button class="md-icon-button" aria-label="Settings">
-                <md-icon><i class="material-icons">menu</i></md-icon>
-            </md-button>
             <h2>
-                <span>Toolbar with Icon Buttons</span>
+                <span>Johnny Magrippis</span>
             </h2>
             <span flex></span>
-            <md-button class="md-icon-button" aria-label="Favorite">
-                <md-icon><i class="material-icons">face</i></md-icon>
+            <md-button class="md-icon-button" aria-label="Home">
+                <md-icon><i class="material-icons">style</i></md-icon>
+                <md-tooltip>
+                    Home
+                </md-tooltip>
             </md-button>
-            <md-button class="md-icon-button" aria-label="More">
-                <md-icon><i class="material-icons">check</i></md-icon>
+            <md-button class="md-icon-button" aria-label="Portfolio">
+                <md-icon><i class="material-icons">assignment_turned_in</i></md-icon>
+                <md-tooltip>
+                    Portfolio
+                </md-tooltip>
+            </md-button>
+            <md-button class="md-icon-button" aria-label="Hobbies">
+                <md-icon><i class="material-icons">favorite</i></md-icon>
+                <md-tooltip>
+                    Hobbies
+                </md-tooltip>
+            </md-button>
+            <md-button class="md-icon-button" aria-label="Blog">
+                <md-icon><i class="material-icons">mode_edit</i></md-icon>
+                <md-tooltip>
+                    Blog
+                </md-tooltip>
             </md-button>
         </div>
     </md-toolbar>
-    <md-content class="md-padding" layout="column">
-        <md-card>
-            <img src="//placehold.it/1920x480" class="md-card-image" alt="Placeholder Image">
-            <md-card-content>
-                <h2 class="md-title">Paracosm</h2>
+    <md-content class="md-padding" layout="column" layout-align="center center">
+        <md-whiteframe class="md-whiteframe-z1 md-padding md-margin workarea" layout="column" layout-align="center center" style="max-width: 1280px">
+            <md-card>
+                <img src="//placehold.it/1920x480" class="md-card-image" alt="Placeholder Image">
+                <md-card-content>
+                    <h2 class="md-title">Paracosm</h2>
 
-                <p>
-                    The titles of Washed Out's breakthrough song and the first single from Paracosm share the
-                    two most important words in Ernest Greene's musical language: feel it. It's a simple request, as
-                    well...
-                </p>
-                <p>@{{ greeting }}</p>
-            </md-card-content>
-            <div class="md-actions" layout="row" layout-align="end center">
-                <md-button>Action 1</md-button>
-                <md-button>Action 2</md-button>
-            </div>
-        </md-card>
+                    <p>
+                        The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                        two most important words in Ernest Greene's musical language: feel it. It's a simple request, as
+                        well...
+                    </p>
+                    <p>@{{ greeting }}</p>
+                </md-card-content>
+                <div class="md-actions" layout="row" layout-align="end center">
+                    <md-button>Action 1</md-button>
+                    <md-button>Action 2</md-button>
+                </div>
+            </md-card>
+            <md-card>
+                <img src="//placehold.it/1920x480" class="md-card-image" alt="Placeholder Image">
+                <md-card-content>
+                    <h2 class="md-title">Paracosm</h2>
+
+                    <p>
+                        The titles of Washed Out's breakthrough song and the first single from Paracosm share the
+                        two most important words in Ernest Greene's musical language: feel it. It's a simple request, as
+                        well...
+                    </p>
+                    <p>@{{ greeting }}</p>
+                </md-card-content>
+                <div class="md-actions" layout="row" layout-align="end center">
+                    <md-button>Action 1</md-button>
+                    <md-button>Action 2</md-button>
+                </div>
+            </md-card>
+        </md-whiteframe>
     </md-content>
 </div>
 <script src="{{ elixir('assets/js/bundle.js') }}"></script>
