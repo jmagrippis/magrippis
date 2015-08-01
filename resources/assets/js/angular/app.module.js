1c0626613f9619951ec081f8b73b5329aeb389e4
Magrippinho <magrippinho@gmail.com>
2015-08-01 16:24:54 +0300

    Adds Summary and Quality sections in CV.

--- resources/assets/js/angular/app.module.js
+++ resources/assets/js/angular/app.module.js
@@ -1,7 +1,7 @@
 var angular = require('angular');
 
 angular
-    .module("magrippis", [require('angular-material'), require('angular-animate'), require('angular-messages'), 'magrippis.routes'])
+    .module("magrippis", [require('angular-material'), require('angular-animate'), require('angular-messages'), require('angular-sanitize'), 'magrippis.routes'])
     .config(['$mdThemingProvider', function ($mdThemingProvider) {
 
         $mdThemingProvider.theme('default')
--- resources/assets/js/angular/components/cv/cvController.js
+++ resources/assets/js/angular/components/cv/cvController.js
@@ -1,33 +1,27 @@
 import { shuffle } from './../../../utility';
+import data from './data.js';
 
 let injections = [];
 
 let controller = function () {
 
-    this.titles = [
-        {name: 'App Developer', active: true},
-        {name: 'Web Designer', active: false},
-        {name: 'Peculiar Photographer', active: false},
-        {name: 'Relentless Writer', active: false},
-        {name: 'Professional Programmer', active: false},
-        {name: 'SQL Samurai', active: false},
-        {name: 'Ninja Javascripter', active: false},
-        {name: 'MongoDB Magician', active: false},
-        {name: 'Stylish Sysadmin', active: false},
-        {name: 'Computer Scientist', active: false}
-    ];
+    this.titles = data.titles;
 
     shuffle(this.titles);
 
     this.currentKey = 0;
 
     this.retitle = () => {
+        this.titles[this.currentKey].active = false;
         if (this.currentKey == this.titles.length - 1) {
             shuffle(this.titles);
             this.currentKey = -1;
         }
         this.currentKey++;
-    }
+        this.titles[this.currentKey].active = true;
+    };
+
+    this.qualities =  data.qualities;
 };
 
 let exports = injections;
