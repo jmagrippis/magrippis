6259dda8d33169e956645fe86116c27ff6727d52
Johnny Magrippis <j@magrippis.com>
2017-11-12 11:01:39 +0200

    Modernize everything

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react';
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <p>You gotta start with a hello, world.</p>
       </div>
     );
   }
