327cfb931b70ef03fcddadf2411291e922f2bc08
Johnny Magrippis <j@magrippis.com>
2018-11-02 19:40:00 +0000

    chore: scaffold Google App Engine deployment of graphql server

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -3,8 +3,8 @@ import { ApolloServer } from 'apollo-server'
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true })
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
 })
