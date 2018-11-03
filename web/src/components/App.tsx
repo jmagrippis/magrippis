4f63e3335e9ee8b3b9f52f82635fbeecda0a386f
Johnny Magrippis <j@magrippis.com>
2018-11-03 14:22:19 +0000

    chore: scaffold app layout

---
+++ web/src/components/App.tsx
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react'
+
+export const App = () => (
+  <Fragment>
+    <header>Johnny Magrippis</header>
+    <div>Ask me about...</div>
+    <footer>Made with 💜 in London</footer>
+  </Fragment>
+)
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,12 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'reset-css'
 
-import { App } from './App'
+import './index.css'
+import { App } from './components/App'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister()
