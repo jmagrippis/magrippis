09ed929077cd02a251f4b7ad32d655249413b574
Johnny Magrippis <j@magrippis.com>
2018-11-03 10:04:59 +0000

    feat: apollo engine

--- graphql/src/index.ts
+++ graphql/src/index.ts
@@ -1,9 +1,21 @@
 import { ApolloServer } from 'apollo-server'
+import { config } from 'dotenv'
 
 import { typeDefs } from './typeDefs'
 import { resolvers } from './resolvers'
 
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true })
+config()
+
+const apolloOptions = {
+  typeDefs,
+  resolvers,
+  introspection: true,
+  engine: {
+    apiKey: process.env.ENGINE_API_KEY
+  }
+}
+
+const server = new ApolloServer(apolloOptions)
 
 server.listen({ port: process.env.PORT }).then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
