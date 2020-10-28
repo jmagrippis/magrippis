b609c6bbc5052991a4367d4b0f568dd3ac399db0
Johnny Magrippis <j@magrippis.com>
2020-10-28 22:02:41 +0000

    purge unused tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,14 @@
 module.exports = {
+  purge: {
+    mode: 'layers',
+    layers: ['components', 'utilities'],
+    content: [
+      './components/**/*.tsx',
+      './components/**/*.css',
+      './pages/**/*.tsx',
+      './pages/**/*.css',
+    ],
+  },
   corePlugins: {
     fontFamily: false,
   },
