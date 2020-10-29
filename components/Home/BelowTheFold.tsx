01872127c60e89b66e10820befb34305cb8adbca
Johnny Magrippis <j@magrippis.com>
2020-10-29 21:02:35 +0000

    dynamically import below the fold content

---
+++ components/Home/BelowTheFold.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Highlights } from './Highlights/Highlights'
+import { Signup } from './Signup'
+
+const BelowTheFold = () => (
+  <>
+    <Highlights />
+    <Signup prompt="If you made it this far, how about subscribing to..." />
+  </>
+)
+
+export default BelowTheFold
--- components/Home/Body.tsx
+++ components/Home/Body.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import dynamic from 'next/dynamic'
 
 import { Hero } from './Hero'
 import { Mission } from './Mission'
 import { CTA } from './CTA'
-import { Highlights } from './Highlights/Highlights'
-import { Signup } from './Signup'
+
+const BelowTheFold = dynamic(() => import('./BelowTheFold'))
 
 export const Body = () => (
   <main className="w-full flex-grow">
     <Hero />
     <Mission />
     <CTA />
-    <Highlights />
-    <Signup prompt="If you made it this far, how about subscribing to..." />
+    <BelowTheFold />
   </main>
 )
