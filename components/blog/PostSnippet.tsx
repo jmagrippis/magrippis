c84588b647cd45651da7885c3a1c9495812da145
Johnny Magrippis <j@magrippis.com>
2020-11-01 10:37:43 +0000

    test blog section and improve a11y

--- components/blog/PostSnippet.tsx
+++ components/blog/PostSnippet.tsx
@@ -18,27 +18,29 @@ export const PostSnippet = ({
     className="cursor-pointer even:bg-gray-100 sm:p-4 sm:border-l first:rounded-tl last:first:rounded-bl border-purple-200 hover:border-purple-300 transition duration-300"
     key={__resourcePath}
   >
-    <Link href={formatMdxPath(__resourcePath)}>
-      <a className="sm:grid sm:grid-cols-3 sm:gap-8">
-        <FeaturedImage title={title} />
-        <div className="p-4 sm:p-0 sm:col-span-2">
-          <h2 className="text-2xl underline mb-2">{title}</h2>
-          <p className="mb-4">{snippet}</p>
-          <div className="flex flex-col sm:flex-row justify-between text-sm text-right">
-            <div className="mb-4 sm:mb-0">
-              {tags.map((tag) => (
-                <span
-                  key={tag}
-                  className="bg-purple-100 text-purple-900 p-2 mr-2 rounded shadow-sm"
-                >
-                  {tag}
-                </span>
-              ))}
+    <article>
+      <Link href={formatMdxPath(__resourcePath)}>
+        <a className="sm:grid sm:grid-cols-3 sm:gap-8">
+          <FeaturedImage title={title} />
+          <div className="p-4 sm:p-0 sm:col-span-2">
+            <h2 className="text-2xl underline mb-2">{title}</h2>
+            <p className="mb-4">{snippet}</p>
+            <div className="flex flex-col sm:flex-row justify-between text-sm text-right">
+              <div className="mb-4 sm:mb-0">
+                {tags.map((tag) => (
+                  <span
+                    key={tag}
+                    className="bg-purple-100 text-purple-900 p-2 mr-2 rounded shadow-sm"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <span>{isoStringToRelativeTime(publishedAt)}</span>
             </div>
-            <span>{isoStringToRelativeTime(publishedAt)}</span>
           </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </article>
   </li>
 )
--- layouts/index.tsx
+++ layouts/index.tsx
@@ -16,25 +16,27 @@ const DefaultLayout = ({
 }: Props) => (
   <div className="bg-prism">
     <main className="m-auto py-8 prose lg:prose-lg xl:prose-xl">
-      <div className="px-4">
-        <h1>{title}</h1>
-        <p className="text-xl lg:text-2xl">{snippet}</p>
-      </div>
-      <FeaturedImage
-        title={title}
-        description={snippet}
-        withSeo
-        className="pt-2 pb-8 lg:pb-12"
-      />
-      <section className="px-4 mb-4">{children}</section>
-      <div className="px-4">
-        <Link href="/blog">
-          <Notice>
-            Check out{' '}
-            <span className="underline text-purple-600">all posts</span>
-          </Notice>
-        </Link>
-      </div>
+      <article>
+        <div className="px-4">
+          <h1>{title}</h1>
+          <p className="text-xl lg:text-2xl">{snippet}</p>
+        </div>
+        <FeaturedImage
+          title={title}
+          description={snippet}
+          withSeo
+          className="pt-2 pb-8 lg:pb-12"
+        />
+        <section className="px-4 mb-4">{children}</section>
+        <div className="px-4">
+          <Link href="/blog" passHref>
+            <Notice>
+              Check out{' '}
+              <span className="underline text-purple-600">all posts</span>
+            </Notice>
+          </Link>
+        </div>
+      </article>
     </main>
   </div>
 )
