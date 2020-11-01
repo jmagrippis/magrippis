114968bd6ecedc7426324a368fb17389b9935835
Johnny Magrippis <j@magrippis.com>
2020-11-01 09:10:42 +0000

    tailwind typography tweaks, use in coding-challenges, remove custom mdx styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,21 +25,43 @@ module.exports = {
       default: {
         css: {
           h1: {
-            fontWeight: theme('fontWeight.default'),
+            fontWeight: theme('fontWeight.normal'),
             marginTop: '0',
           },
           h2: {
-            fontWeight: theme('fontWeight.default'),
+            fontWeight: theme('fontWeight.normal'),
             marginTop: '0',
           },
           h3: {
-            fontWeight: theme('fontWeight.default'),
+            fontWeight: theme('fontWeight.normal'),
             marginTop: '0',
+          },
+          a: {
+            color: theme('colors.purple.600'),
+            '&:hover': {
+              color: theme('colors.purple.800'),
+            },
           },
           img: {
             margin: '0 auto',
             boxShadow: theme('boxShadow.md'),
             borderRadius: theme('borderRadius.default'),
+          },
+          video: {
+            margin: '0 auto',
+            boxShadow: theme('boxShadow.md'),
+            borderRadius: theme('borderRadius.default'),
+          },
+          pre: {
+            fontFamily: '"Fira Code", monospace',
+          },
+          code: {
+            backgroundColor: theme('colors.purple.100'),
+            color: theme('colors.purple.900'),
+            fontFamily: '"Fira Code", monospace',
+            fontWeight: theme('fontWeight.normal'),
+            paddingLeft: theme('padding.2'),
+            paddingRight: theme('padding.2'),
           },
         },
       },
