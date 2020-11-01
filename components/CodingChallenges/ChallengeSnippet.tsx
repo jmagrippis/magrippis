29c3a2124bae54e38ebfd31ad6c0acfa240a2b3f
Johnny Magrippis <j@magrippis.com>
2020-11-01 10:46:03 +0000

    test coding challenges section and improve a11y

--- components/CodingChallenges/ChallengeSnippet.tsx
+++ components/CodingChallenges/ChallengeSnippet.tsx
@@ -18,29 +18,31 @@ export const ChallengeSnippet = ({
     className="cursor-pointer sm:p-4 border-l border-purple-200 hover:border-purple-300 transition duration-300"
     key={__resourcePath}
   >
-    <Link href={formatMdxPath(__resourcePath)}>
-      <a className="sm:grid sm:grid-cols-3 sm:gap-8">
-        <div className="bg-gray-100 rounded shadow flex justify-center items-center p-1">
-          <FeaturedPreview title={title} />
-        </div>
-        <div className="p-4 sm:p-0 sm:col-span-2">
-          <h2 className="text-2xl underline mb-2">{title}</h2>
-          <p className="mb-4">{snippet}</p>
-          <div className="flex flex-col sm:flex-row justify-between text-sm text-right">
-            <div className="mb-4 sm:mb-0">
-              {tags.map((tag) => (
-                <span
-                  key={tag}
-                  className="text-purple-900 p-2 mr-2 rounded shadow-sm"
-                >
-                  {tag}
-                </span>
-              ))}
-            </div>
-            <span>{isoStringToRelativeTime(publishedAt)}</span>
+    <article>
+      <Link href={formatMdxPath(__resourcePath)}>
+        <a className="sm:grid sm:grid-cols-3 sm:gap-8">
+          <div className="bg-gray-100 rounded shadow flex justify-center items-center p-1">
+            <FeaturedPreview title={title} />
           </div>
-        </div>
-      </a>
-    </Link>
+          <div className="p-4 sm:p-0 sm:col-span-2">
+            <h2 className="text-2xl underline mb-2">{title}</h2>
+            <p className="mb-4">{snippet}</p>
+            <div className="flex flex-col sm:flex-row justify-between text-sm text-right">
+              <div className="mb-4 sm:mb-0">
+                {tags.map((tag) => (
+                  <span
+                    key={tag}
+                    className="text-purple-900 p-2 mr-2 rounded shadow-sm"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+              <span>{isoStringToRelativeTime(publishedAt)}</span>
+            </div>
+          </div>
+        </a>
+      </Link>
+    </article>
   </li>
 )
