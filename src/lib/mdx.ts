1072a6525c6f1be0c28bf8d0b503fbc9927e841d
Johnny Magrippis <j@magrippis.com>
2020-12-06 15:26:00 +0000

    💡 How to add a custom Tweet CTA to your Next.js page

--- src/lib/mdx.ts
+++ src/lib/mdx.ts
@@ -3,6 +3,7 @@ import rehypePrism from '@mapbox/rehype-prism'
 import { Demonstration } from 'components/blog/Demonstration'
 import { RelativeTimeDemo } from 'components/blog/RelativeTimeDemo/RelativeTimeDemo'
 import { FeaturedPreview } from 'components/CodingChallenges/2020/CyclingElement/FeaturedPreview'
+import { ShareCTA } from 'components/blog/ShareCTADemo'
 import { Video } from 'components/Video/Video'
 
 export const BLOG_PATH_PREFIX = 'src/pages/blog/'
@@ -13,6 +14,7 @@ export const mdxOptions = { rehypePlugins: [rehypePrism] }
 export const blogComponents = {
   Demonstration,
   RelativeTimeDemo,
+  ShareCTA,
 }
 
 export const challengeComponents = {
