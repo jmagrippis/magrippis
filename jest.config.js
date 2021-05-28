d490d64a2a34b18a60b6ad1c4f590bce25fec115
Johnny Magrippis <j@magrippis.com>
2021-05-28 12:54:55 +0100

    ⬆️  upgrade non-breaking dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   clearMocks: true,
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['./setupTests.ts'],
   moduleDirectories: ['node_modules', 'src'],
   testPathIgnorePatterns: ['/node_modules/', '/cypress/'],
   transform: {
     '^.+\\.(j|t)sx?$': 'babel-jest',
-    '^.+\\.svg$': 'jest-svg-transformer',
+    '\\.svg$': 'svg-jest',
   },
 }
