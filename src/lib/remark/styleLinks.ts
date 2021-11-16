c66cae898a8d5cdafdbf045fe198f1d7ac38dd3a
Johnny Magrippis <j@magrippis.com>
2021-11-16 19:34:06 +0000

    ⬆️  upgrade most dependencies

--- src/lib/remark/styleLinks.ts
+++ src/lib/remark/styleLinks.ts
@@ -1,11 +1,13 @@
 import type { Plugin } from 'unified'
-import visit from 'unist-util-visit'
+import { Node, visit } from 'unist-util-visit'
 
-export const styleLinks: Plugin = () =>
+const styleLinks: Plugin = () =>
   function transform(tree) {
-    visit(tree, 'link', (linkNode) => {
+    visit(tree, 'link', (linkNode: Node) => {
       linkNode.data = {
         hProperties: { className: 'underline text-purple-400' },
       }
     })
   }
+
+export default styleLinks
