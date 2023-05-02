da5aa9aca590a4eb53651b42d0421bef2329dbbb
Johnny Magrippis <j@magrippis.com>
2023-05-02 18:32:09 +0200

    ✨  add typing animation of titles

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -1,6 +1,29 @@
 import Image from 'next/image'
 
 import avatar from './hero.jpg'
+import Typing from './Typing'
+import {titles} from './titles'
+
+const shuffleArray = (array: any[]) => {
+	const shuffledArray = [...array]
+	let currentIndex = shuffledArray.length,
+		randomIndex
+
+	// While there remain elements to shuffle.
+	while (currentIndex != 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex)
+		currentIndex--
+
+		// And swap it with the current element.
+		;[shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
+			shuffledArray[randomIndex],
+			shuffledArray[currentIndex],
+		]
+	}
+
+	return shuffledArray
+}
 
 const Hero = () => (
 	<section className="container mb-4 grid grid-cols-12 items-center gap-4 px-2 py-4 lg:gap-8">
@@ -12,7 +35,8 @@ const Hero = () => (
 			className="col-span-3 rounded-full shadow-mid lg:col-span-2"
 		/>
 		<h1 className="col-span-9 text-2xl lg:col-span-10 lg:text-4xl">
-			Hello, I’m <strong>Johnny</strong> and...
+			Hello, I’m <strong>Johnny</strong> and I’m...{' '}
+			<Typing titles={shuffleArray(titles)} />
 		</h1>
 	</section>
 )
---
+++ src/app/Hero/Typing.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import {useEffect, useState} from 'react'
+
+const MIN_TYPING_MS = 70
+const MAX_TYPING_MS = 110
+const DELETING_MS = 50
+const PAUSED_MS = 1500
+
+type TypingState = 'typing' | 'deleting' | 'paused'
+
+type Props = {
+	titles: { article: string, title: string}[]
+}
+
+const Typing = ({titles}: Props) => {
+	const [typingState, setTypingState] = useState<TypingState>('typing')
+	const [selectedTitleIndex, setSelectedTitleIndex] = useState(0)
+	const [typedLength, setTypedLength] = useState(0)
+
+	useEffect(() => {
+		const createNextTimeout = ({
+			typedLength,
+			typingState,
+			selectedTitleIndex,
+		}: {
+			typedLength: number
+			typingState: TypingState
+			selectedTitleIndex: number
+		}) => {
+			switch (typingState) {
+				case 'typing':
+					return setTimeout(() => {
+						setTypedLength(typedLength + 1)
+						if (typedLength + 1 === titles[selectedTitleIndex].title.length) {
+							setTypingState('paused')
+						}
+					}, Math.floor(Math.random() * (MAX_TYPING_MS - MIN_TYPING_MS + 1) + MIN_TYPING_MS))
+				case 'deleting':
+					return setTimeout(() => {
+						const nextTitleIndex = titles[selectedTitleIndex + 1]
+							? selectedTitleIndex + 1
+							: 0
+						if (
+							titles[selectedTitleIndex].title.slice(0, typedLength) ===
+							titles[nextTitleIndex].title.slice(0, typedLength)
+						) {
+							setSelectedTitleIndex(nextTitleIndex)
+							setTypingState('typing')
+							return
+						}
+
+						setTypedLength(typedLength - 1)
+					}, DELETING_MS)
+				case 'paused':
+					return setTimeout(() => {
+						setTypingState('deleting')
+					}, PAUSED_MS)
+				default:
+					break
+			}
+		}
+
+		const timeout = createNextTimeout({
+			typedLength,
+			typingState,
+			selectedTitleIndex,
+		})
+
+		return () => clearTimeout(timeout)
+	}, [typedLength, typingState, selectedTitleIndex, titles])
+
+	const {article, title} = titles[selectedTitleIndex]
+	return (
+		<>
+			{article}{' '}
+			<strong
+				className={`text-emphasis after:content-['|'] ${
+					typingState === 'paused' ? 'after:animate-blink' : ''
+				}`}
+			>
+				{title.slice(
+					0,
+					typedLength
+				)}
+			</strong>
+		</>
+	)
+}
+
+export default Typing
