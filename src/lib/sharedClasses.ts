a4077edccc8b3896a033858605508f36af3487a8
Johnny Magrippis <j@magrippis.com>
2023-10-23 21:09:16 +0200

    ✨ Add About page

---
+++ src/lib/sharedClasses.ts
@@ -0,0 +1,2 @@
+export const proseClasses =
+	'prose prose-lg prose-stone pt-10 dark:prose-invert lg:prose-xl xl:prose-2xl prose-a:decoration-emphasis prose-a:decoration-2 prose-a:transition-colors hover:prose-a:text-emphasis hover:prose-a:decoration-emphasis-hover prose-code:rounded-sm prose-code:bg-surface-2 prose-pre:p-0 prose-img:mx-auto md:prose-img:rounded md:prose-img:shadow-low'
