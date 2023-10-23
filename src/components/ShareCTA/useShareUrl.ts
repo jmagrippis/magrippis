c94514925f1e1853bbf6f053b0e6d711abcdfd4a
Johnny Magrippis <j@magrippis.com>
2023-10-23 16:42:05 +0200

    🐛 use NEXT_PUBLIC_ROOT_URL

--- src/components/ShareCTA/useShareUrl.ts
+++ src/components/ShareCTA/useShareUrl.ts
@@ -1,11 +1,11 @@
 import {usePathname} from 'next/navigation'
 
-export const rootUrl = process.env.VERCEL_URL
-	? `https://${process.env.VERCEL_URL}`
-	: `http://localhost:${process.env.PORT || 3000}`
-
 export const useShareUrl = () => {
 	const pathname = usePathname()
 
-	return `${rootUrl}${pathname}`
+	const rootUrl = process.env.NEXT_PUBLIC_ROOT_URL
+		? `https://${process.env.NEXT_PUBLIC_ROOT_URL}`
+		: `http://localhost:${process.env.PORT || 3000}`
+
+	return `${rootUrl}/${pathname}`
 }
