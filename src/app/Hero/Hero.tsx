ebb56e695fac7a0d9820cb4c635d5c401e357d8d
Johnny Magrippis <j@magrippis.com>
2023-10-26 10:59:03 +0200

    ✨ YouTube & Testimonial sections

--- src/app/Hero/Hero.tsx
+++ src/app/Hero/Hero.tsx
@@ -6,7 +6,7 @@ import {titles} from './titles'
 import Typing from './Typing'
 
 const Hero = () => (
-	<section className="container mb-8 mt-4 grid h-48 grid-cols-12 items-center gap-4 px-2 py-4 lg:h-60 lg:gap-8">
+	<section className="container mb-8 mt-4 grid h-48 grid-cols-12 items-center gap-4 px-2 py-4 lg:mb-16 lg:mt-12 lg:h-60 lg:gap-8">
 		<Image
 			src={avatar}
 			alt="Johnny’s avatar"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,29 @@ import Image from 'next/image'
 import livestreamCover from './streaming.jpg'
 
 import Hero from './Hero/Hero'
+import {Mission} from './Mission/Mission'
+import {YouTubeSection} from './YouTubeSection/YouTubeSection'
+import {Testimonials} from './Testimonials/Testimonials'
+import {Paragraph} from './Paragraph'
+import {OtherWork} from './OtherWork/OtherWork'
 
 const Home = () => (
 	<main>
 		<Hero />
+		<Mission />
+		<YouTubeSection />
+		<Testimonials />
+		<section className="mb-4 flex flex-col gap-4">
+			<Paragraph>
+				The best way to support me is to watch my videos, tune into my
+				livestreams 🙂
+			</Paragraph>
+			<Paragraph>
+				For the extra mile, leave a comment: some feedback, whether I made you
+				laugh, or helped you learn... or what you’d like to see me cover next!
+			</Paragraph>
+		</section>
+		<OtherWork />
 		<section className="container">
 			<p className="px-2">Watch videos & streams!</p>
 			<Image
