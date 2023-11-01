ba092fdba555d9d646ad175c48bad3a4ab96ac40
Johnny Magrippis <j@magrippis.com>
2023-11-01 18:43:08 +0100

    💚 test against deployment

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:3000',
+		baseURL: process.env.NEXT_PUBLIC_ROOT_URL || 'http://localhost:3000',
 
 		launchOptions: {
 			// debug failures better by having Playwright
@@ -75,9 +75,11 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: 'bun run start',
-		port: 3000,
-		reuseExistingServer: !process.env.CI,
-	},
+	webServer: !process.env.CI
+		? {
+				command: 'bun run dev',
+				port: 3000,
+				reuseExistingServer: true,
+		  }
+		: undefined,
 })
