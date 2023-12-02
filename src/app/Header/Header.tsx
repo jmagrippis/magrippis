3f81a18dea2e3732fc90fdc3cd30131345e7f95e
Johnny Magrippis <j@magrippis.com>
2023-12-02 16:50:33 +0100

    💄  more contrast for accessibility

--- src/app/Header/Header.tsx
+++ src/app/Header/Header.tsx
@@ -1,7 +1,7 @@
 import {Nav} from './Nav'
 
 export const Header = () => (
-	<header className="h-[var(--header-height)] bg-gradient-to-br from-primary-700 to-primary-900">
+	<header className="h-[var(--header-height)] bg-gradient-to-br from-primary-800 to-primary-950">
 		<Nav />
 	</header>
 )
