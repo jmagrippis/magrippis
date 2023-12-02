0468d0795a68c493ccab62e6b6c6ef5beb5483ec
Johnny Magrippis <j@magrippis.com>
2023-12-02 16:40:33 +0100

    ♻️ more logic for toggling the theme in the inline JS instead of the button

--- public/theme-toggle.js
+++ public/theme-toggle.js
@@ -1,13 +1,36 @@
 const STORAGE_KEY = 'theme'
+// default to dark mode, because **I** like it more :-)
+const defaultTheme = 'dark'
 
-const getColorPreference = () => {
-	const currentTheme = localStorage.getItem(STORAGE_KEY)
-	if (currentTheme) return currentTheme
+const getTheme = () => {
+	if (typeof localStorage !== 'undefined') {
+		return localStorage.getItem('theme') || defaultTheme
+	}
+	return defaultTheme
+}
 
-	return window.matchMedia('(prefers-color-scheme: dark)').matches
-		? 'dark'
-		: 'light'
+const setTheme = (theme) => {
+	document.documentElement.dataset.theme = theme
 }
 
 // set early so no page flashes / CSS is made aware
-document.documentElement.dataset.theme = getColorPreference()
+setTheme(getTheme())
+
+const themeObserver = new MutationObserver(() => {
+	const nextTheme = document.documentElement.dataset.theme
+	if (nextTheme) {
+		localStorage.setItem('theme', nextTheme)
+	}
+})
+themeObserver.observe(document.documentElement, {
+	attributes: true,
+	attributeFilter: ['data-theme'],
+})
+
+const toggleTheme = () => {
+	const currentTheme = getTheme()
+	const nextTheme = currentTheme === 'dark' ? 'light' : 'dark'
+	setTheme(nextTheme)
+}
+
+window.toggleTheme = toggleTheme
